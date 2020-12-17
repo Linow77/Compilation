5fba7f4a4afd851e88dc52eeec06d81a6e055268
Linow <cloos.richard@gmail.com>
2020-12-17 23:06:26 +0100

    lecture du sujet

---
+++ test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+
+	char* chaine = "";
+	char* chaine2 = NULL;
+	int i=0;
+	/*do{
+		printf("lettre: %c\n",chaine[i]);
+		i++;
+	}
+	while (chaine[i] != '\0');*/
+	printf("lettre1: %c\n",chaine);
+	printf("lettre2: %c\n",chaine2);
+	return 1;
+}
