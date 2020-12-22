cc78b51ddf3294260640b3213bf9ed11992d0305
Linow77 <46446687+Linow77@users.noreply.github.com>
2020-12-22 15:37:29 +0100

    ajout 3 premières fonction de création d'automate

--- projet.c
+++ projet.c
@@ -1,8 +1,148 @@
 #include "projet.h"
 
+
 int main() {
 
-	printf("hello world\n");
+	AUTOMATEAFN afnVide,afnMotVide,afncaractere;
+
+	afnVide = langageVide();
+	printf("/**** Langage Vide ****/\n");
+	AfficherAutomate(afnVide);
+
+	afnMotVide = langageMotVide();
+	printf("\n/**** Langage Mot Vide ****/\n");
+	AfficherAutomate(afnMotVide);
+
+	afncaractere = langagecaractere('a');
+	printf("\n/**** Langage Caractere ****/\n");
+	AfficherAutomate(afncaractere);
 	return 1;
 
 }
+
+void AfficherAutomate(AUTOMATEAFN afn){
+	int i=0;
+	int tailleQ = (sizeof(afn.Q)/sizeof(unsigned int));
+	int tailleF = (sizeof(afn.F)/sizeof(int));
+	int tailleD = (sizeof(afn.D)/sizeof(char*));
+	int tailleZ;
+
+	//Q
+	//printf("taille de Q: %d\n",tailleQ);
+	if(afn.Q!=NULL){
+		printf("Ensemble d'etats (Q): ");
+		for(i=0;i<tailleQ;i++){
+			printf("%u, ",afn.Q[i]);
+		}	
+		printf("\n");
+	}
+
+	//Z
+
+	if(afn.Z == NULL){
+		printf("Alphabet de l'automate (Z): vide\n");
+	} else if(!strcmp(afn.Z,"")){
+		printf("Alphabet de l'automate (Z): mot vide\n");
+	}else{
+		tailleZ = strlen(afn.Z);
+		printf("Alphabet de l'automate (Z): ");
+		for(i=0;i<tailleZ;i++){
+			printf("%c, ",afn.Z[i]);
+		}
+		printf("\n");
+	}
+	
+
+	//s
+	printf("Etat initial (s): %u\n",afn.s);
+
+	//F
+	//printf("taille de F: %d\n",tailleF);
+	if(afn.F!=NULL){
+		printf("Ensemble d'etats accepteurs (F): ");
+		for(i=0;i<tailleF;i++){
+			printf("%d, ",afn.F[i]);
+		}
+		printf("\n");
+	}else{
+		printf("Aucun etat accepteur\n");
+	}
+
+	//D
+	//printf("taille de D: %d\n",tailleD);
+	if(afn.D!=NULL){
+		printf("Ensemble des Ã©tats transitions (D): ");
+		for(i=0;i<tailleD;i++){
+			printf("%s, ",afn.D[i]);
+		}
+		printf("\n");	
+	}else{
+		printf("Aucune transition\n");
+	}
+}
+
+/**AUTOMATE FINI NON DETERMINISTES **/
+AUTOMATEAFN langageVide(){
+	AUTOMATEAFN afn;
+
+	afn.Q = (unsigned int*)malloc(sizeof(unsigned int));
+	afn.Q[0] = 0;
+
+	afn.Z = NULL;
+
+	afn.s = 0;
+
+	afn.F = NULL;
+
+	afn.D = NULL;
+
+	return afn;
+}
+
+AUTOMATEAFN langageMotVide(){
+	AUTOMATEAFN afn;
+	afn.Q = (unsigned int*)malloc(sizeof(unsigned int));
+	afn.Q[0] = 0;
+
+	afn.Z = "";
+
+	afn.s = 0;
+
+	afn.F = (int*)malloc(sizeof(int));
+	afn.F[0] = 0;
+
+	afn.D = NULL;
+
+	return afn;
+}
+
+AUTOMATEAFN langagecaractere(char caractere){
+	AUTOMATEAFN afn;
+	afn.Q = (unsigned int*)malloc(sizeof(unsigned int)*2);
+	afn.Q[0] = 0;
+	afn.Q[1] = 1;
+
+	afn.Z = (char*)malloc(sizeof(char));	// allouer que la taille d'un char ?
+	afn.Z[0] = caractere;
+	afn.Z[1] = '\0';
+
+	afn.s = 0;
+
+	afn.F = (int*)malloc(sizeof(int));
+	afn.F[0] = 1;
+
+	afn.D = (char**)malloc(sizeof(char*));
+	afn.D[0] = (char*)malloc(sizeof(char)); // allouer que la taille d'un char ?
+	//creation de la chaine "etat1-caractere-etat2"
+	char * chaine =NULL;
+	chaine = (char*)malloc(sizeof(char));
+	chaine[0] = '0';
+	chaine[1] = caractere;
+	chaine[2] = '1';
+	chaine[3] = '\0';
+	afn.D[0]=chaine;
+
+	return afn;
+}
+
+/**AUTOMATE FINI NON DETERMINISTES PLUS EVOLUES **/
